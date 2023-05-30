aa2e4bf43607d0a54d1d063e851c26966ddc1fc2
yunsangjun <ysj188cm@gmail.com>
2023-05-30 13:18:26 +0000

    Do not consider kubernetes version in collector and detector set

--- cases/const.go
+++ cases/const.go
@@ -7,8 +7,3 @@ const (
 const (
 	CONFIG_KUBECONFIG = "kubeconfig"
 )
-
-const (
-	K8S_VERSION_1_21 = 1.21
-	K8S_VERSION_1_25 = 1.25
-)
--- cases/detector_set.go
+++ cases/detector_set.go
@@ -1,11 +1,8 @@
 package cases
 
 import (
-	"strconv"
-
 	"github.com/kakao/detek/cases/detector"
 	"github.com/kakao/detek/pkg/detek"
-	"github.com/kakao/detek/pkg/utils"
 )
 
 type DetectorSetInitiator func(map[string]string) []detek.Detector
@@ -13,7 +10,7 @@ type DetectorSetInitiator func(map[string]string) []detek.Detector
 var (
 	DetectorSet map[string]DetectorSetInitiator = map[string]DetectorSetInitiator{
 		DefaultSet: func(m map[string]string) []detek.Detector {
-			detectors := []detek.Detector{
+			return []detek.Detector{
 				&detector.FailedPod{},
 				&detector.PodWithoutLimits{
 					DoNotCheckCPU:    true, // Disable Checking CPU Limits
@@ -27,22 +24,8 @@ var (
 				&detector.PodWithoutReadinessProbe{},
 				&detector.ServiceNoAvailableTarget{},
 				&detector.ServicePartiallyAvailable{},
+				&detector.ApiLifecyclePolicyV1Beta1{},
 			}
-
-			var err error
-			k8sVersionInfo, err := utils.GetK8sVersion(m[CONFIG_KUBECONFIG])
-			if err != nil {
-				return nil
-			}
-			k8sVersion, err := strconv.ParseFloat(k8sVersionInfo.Major+"."+k8sVersionInfo.Minor, 64)
-			if err != nil {
-				return nil
-			}
-			if k8sVersion >= K8S_VERSION_1_21 && k8sVersion < K8S_VERSION_1_25 {
-				detectors = append(detectors, &detector.ApiLifecyclePolicyV1Beta1{})
-			}
-
-			return detectors
 		},
 		// add more preset here
 	}
